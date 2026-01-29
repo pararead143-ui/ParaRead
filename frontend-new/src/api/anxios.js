3f38347ea485a6fd7dbd3d2ebd5255a8cd02a220
Pararead <pararead143gmail.com>
2026-01-29 21:39:18 +0800

    Final Fix

--- frontend-new/src/api/anxios.js
+++ frontend-new/src/api/anxios.js
@@ -3,10 +3,7 @@ import axios from "axios";
 // Create an Axios instance with the base URL of your Django backend
 const instance = axios.create({
   baseURL: "https://pararead-backend.onrender.com/api/",
- // replace with your backend URL if different
-  headers: {
-    "Content-Type": "application/json",
-  },
+ 
 });
 
 // Add a request interceptor to attach JWT access token
